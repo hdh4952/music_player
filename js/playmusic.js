2ed3f743f62637857283c1eda64313f0f5b62ed7
hdh4952 <63997481+hdh4952@users.noreply.github.com>
2022-08-14 23:28:29 +0900

    no copyright music

--- js/playmusic.js
+++ js/playmusic.js
@@ -1,11 +1,20 @@
+import song from "./song.js";
+
+console.log(song);
 const playBtn = document.querySelector(".main__playBtn");
 const bgImg = document.querySelector(".main__album");
 bgImg.style.animationPlayState = "paused";
+
+const audio = new Audio(song[0].src);
+audio.play();
+
 
 onClickPlayBtn = (e) => {
     e.target.classList.toggle("fa-circle-play");
     e.target.classList.toggle("fa-circle-pause");
     bgImg.style.animationPlayState = bgImg.style.animationPlayState == "paused" ? "running" : "paused";
+
+
 }
 
 playBtn.addEventListener("click", onClickPlayBtn);
---
+++ js/song.js
@@ -0,0 +1,12 @@
+export const song = [
+    {
+        "artist": "Tathiche",
+        "title": "Raging Feelings",
+        "src": "./js/song/FLERO-shade.mp3"
+    },
+    {
+        "artist": "FLERO",
+        "title": "shade",
+        "src": "./js/song/Tatiche-Raging Feelings.mp3"
+    }
+]
